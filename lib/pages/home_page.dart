f188a39f5dfde714d598671b45f40a7850d316c6
wano-77 <zayno.w.z@gmail.com>
2023-03-21 17:13:44 -0800

    making heading & search seaction

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tripadvisor/components/heading.dart';
+import 'package:tripadvisor/components/search.dart';
+import 'package:tripadvisor/utils/styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      backgroundColor: background,
+body: SafeArea(
+  child: Padding(
+    padding: EdgeInsets.only(left: medium,top: 50,right: medium),
+    child: Column(
+      children: [
+           const HeadingSection(),
+           SizedBox(height: medium),
+           const SearchSection(),
+      ],
+    ),
+  ),
+  
+  ),
 
     );
   }
